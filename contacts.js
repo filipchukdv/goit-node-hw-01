87c0f0b74d2104d1f084ee38a25275fc8208025f
filipchukdv <filipchukdv@gmail.com>
2023-07-08 21:07:15 +0300

    Initial commit

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contactPath = require("./db/contacts.json");
+
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
